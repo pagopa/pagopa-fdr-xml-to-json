fa714168a6df5a1f538cfa8c075f22012c90fd81
Francesco Parisi <francesco.parisi@tasgroup.eu>
2024-05-23 17:57:06 +0200

    NOD-892: [FDR] XML-to-JSON - Implementazione JUnit

--- src/test/java/FdrXmlToJsonTest.java
+++ src/test/java/FdrXmlToJsonTest.java
@@ -20,6 +20,7 @@ import util.TestUtil;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.UUID;
@@ -40,8 +41,13 @@ public class FdrXmlToJsonTest {
 
     static void setFinalStatic(Field field, Object newValue) throws Exception {
         field.setAccessible(true);
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
         field.set(null, newValue);
     }
+
+
 
     @Test
     @SneakyThrows
