9f590fd9b0671d7451ee917777e065942ddce89a
Francesco <cesareo.francesco@gmail.com>
2025-03-10 18:33:23 +0100

    fix info

--- src/main/java/it/gov/pagopa/fdrxmltojson/Info.java
+++ src/main/java/it/gov/pagopa/fdrxmltojson/Info.java
@@ -5,7 +5,12 @@ import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 
+import java.io.InputStream;
 import java.util.Optional;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import it.gov.pagopa.fdrxmltojson.dto.AppInfo;
 
 
 /**
@@ -25,8 +30,34 @@ public class Info {
 			authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
 			final ExecutionContext context) {
 
-		return request.createResponseBuilder(HttpStatus.OK).build();
+		return request
+				.createResponseBuilder(HttpStatus.OK)
+				.header("Content-Type", "application/json")
+				.body(
+						getInfo(
+								context.getLogger(),
+								"/META-INF/maven/it.gov.pagopa.fdrxmltojson/pom.properties"))
+				.build();
 	}
 
+	public synchronized AppInfo getInfo(Logger logger, String path) {
+		String version = null;
+		String name = null;
+		try {
+			Properties properties = new Properties();
+			InputStream inputStream = loadResource(path);
+			if (inputStream != null) {
+				properties.load(inputStream);
+				version = properties.getProperty("version", null);
+				name = properties.getProperty("artifactId", null);
+			}
+		} catch (Exception e) {
+			logger.severe("Impossible to retrieve information from pom.properties file.");
+		}
+		return AppInfo.builder().version(version).environment("azure-fn").name(name).build();
+	}
 
+	public InputStream loadResource(String path) {
+		return getClass().getResourceAsStream(path);
+	}
 }
